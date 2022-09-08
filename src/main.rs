664d01fb6b24115eb99e4231c1e78d649e5b1470
Joshua Gilman <joshuagilman@gmail.com>
2022-09-08 09:27:25 -0700

    feat: adds say_hello function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Usage: {} <name>", args[0]);
+        return;
+    }
+
+    println!("{}", say_hello(&args[1]));
+}
+
+fn say_hello(name: &str) -> String {
+    return format!("Hello, {}!", name);
 }
